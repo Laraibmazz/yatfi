af051fa10ce016fb5decd644ba1e221804ca95ed
Rajab <rajabm259@gmail.com>
2021-01-24 15:06:15 +0500

    the static

--- index.js
+++ index.js
@@ -1,28 +1,85 @@
-var express = require('express')
-var mongoose = require('mongoose')
-const { router } = require('./scripts/assets')
-require('dotenv').config()
+// Requrie modules
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-con = mongoose.connection
+var express = require("express");
+var mongoose = require("mongoose");
+const {} = require("./scripts/assets");
+var bodyParser = require("body-parser");
+var path = require("path");
+var cookieSession = require("cookie-session");
+var socket = require('socket.io')
 
-con.once('open', function() {
-  schema = new mongoose.Schema({
-    name: String  
+
+// config
+
+require("dotenv").config();
+app = express();
+var u = bodyParser.urlencoded({ extended: true });
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["test"],
+    // Cookie Options
+    maxAge: 1000, // 24 hours
   })
-  const song = mongoose.model('Alter', schema, 'alter')
-  song.find({Activated: true}, function(err, res) {
-    console.log(res);
-  })
-})
-app = express()
+);
 
-app.use('/', express.static('./static/examples/'))
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+con = mongoose.connection;
 
-app.use('/assets', router)
+// Middlewares
 
-app.use('/test', function(req,res) {
-  res.send('heelo')
-})
+app.use("/", express.static("./static/examples/"));
+app.use("/assets", router);
+app.use("/register", express.static("./static/examples/reg.html"));
+app.use("/login", express.static("./static/examples/login.html"));
+app.use("/admin", function (req, res) {
+  if (req.session.test == "tfestfr") {
+    res.sendFile(path.join(__dirname + "/static/examples/dashboard.html"));
+  } else {
+    res.send("You are not logged in");
+  }
+});
 
-app.listen(process.env.PORT || 8000)
+// Login form
+
+app.post("/login", u, function (req, res) {
+  var em = req.body.email;
+  var pwd = req.body.pwd;
+  con.once("open", function () {
+    schema = new mongoose.Schema({
+      name: String,
+      email: String,
+      pwd: String,
+    });
+    const song = mongoose.model("Alter", schema, "alter");
+    song.find({ email: em }, function (err, db) {
+      if (err) return err;
+      if (db[0].email == em && db[0].pwd == pwd) {
+        req.session.test = "tfestfr";
+        res.redirect("/admin");
+      }
+    });
+  });
+});
+
+// Register form
+
+app.post("/register", u, function (req, res) {
+  res.send(req.body);
+});
+
+// Listen to Port
+
+var PORT = process.env.PORT || 80
+server = app.listen(PORT, function() {
+  console.log(`connected on ${PORT}`)
+});
+
+var io = socket(server)
+
+io.on('connection', () => {
+  console.log('a user connected');
+});
