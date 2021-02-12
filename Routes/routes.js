2107c3c8ef016b27c328769a3f332eae449f99c0
Rajab <rajabm259@gmail.com>
2021-02-12 22:42:35 +0500

    file structure

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,12 +1,11 @@
 var express = require('express')
-var path = require('path')
 var { register } = require('./register')
 var { login } = require('./login')
 var { app } = require('./admin')
 
 router = express.Router()
 
-router.use(function (req, res, next) {
+router.use("/", function (req, res, next) {
     if (req.protocol == 'http' && req.hostname != 'localhost') {
         res.redirect('https://yatfi.herokuapp.com/')
     }
@@ -14,8 +13,7 @@ router.use(function (req, res, next) {
         next()
     }
 })
-
-router.use('/', express.static(path.join(__dirname + '/../static/examples/index.html')))
+router.use("/", express.static(__dirname + '/../static/examples/'))
 
 router.use("/assets", express.static(__dirname + '/../static/assets/'))
 
